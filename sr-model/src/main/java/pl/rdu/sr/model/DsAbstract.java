30c294b6657d56e23e32d97a908cdad816ab1234
renf7_000 <renf7_000@rafalduda-nb>
2014-11-16 15:30:56 +0100

    changing a project struckture

---
+++ sr-model/src/main/java/pl/rdu/sr/model/DsAbstract.java
@@ -0,0 +1,13 @@
+package pl.rdu.sr.model;
+
+public class DsAbstract {
+    private long id;
+    
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+}
---
+++ sr-model/src/main/java/pl/rdu/sr/model/DsWordSnapshot.java
@@ -0,0 +1,20 @@
+package pl.rdu.sr.model;
+
+/**
+ * Entity to hold information about snapshots.
+ * 
+ * @author renf7_000
+ *
+ */
+public class DsWordSnapshot extends DsAbstract {
+    /** Snapshot duration in minisecounds. */
+    private long duration;
+
+    public long getDuration() {
+        return duration;
+    }
+
+    public void setDuration(long duration) {
+        this.duration = duration;
+    }
+}
